c9e52c18e256453ad9676ef2de85ff315708d00b
tejap <btejapramod@gmail.com>
2022-02-28 12:34:19 +0530

    New Changes

--- name.tf
+++ name.tf
@@ -1,6 +1,6 @@
 resource "aws_ec2_tag" "tag" {
   count       = var.RESOURCE_ID_COUNT
-  resource_id = element(var.RESOURCE_ID_COUNT, count.index)
+  resource_id = element(var.ALL_TAG_IDS, count.index)
   key         = var.TAG_NAME
   value       = var.TAG_VALUE
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,4 @@ variable "TAG_NAME" {}
 variable "TAG_VALUE" {}
 variable "RESOURCE_ID_COUNT" {}
 variable "ENV" {}
+variable "ALL_TAG_IDS" {}
